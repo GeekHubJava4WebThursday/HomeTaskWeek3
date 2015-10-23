a6ec7bcbd2a1fbd92564cc9b43028f0a23c43873
Nicholas.Morhun <Nicksbox78@gmail.com>
2015-10-23 18:14:54 +0300

    Implemented sort method in ArraySorter

--- src/com/geekhub/hw3/comparable/ArraySorter.java
+++ src/com/geekhub/hw3/comparable/ArraySorter.java
@@ -2,6 +2,7 @@ package com.geekhub.hw3.comparable;
 
 import java.lang.Comparable;
 import java.util.Arrays;
+import java.util.Random;
 
 public class ArraySorter {
 
@@ -18,7 +19,59 @@ public class ArraySorter {
      * @return sorted array
      */
     public static Comparable[] sort(Comparable[] elements) {
-        //TODO: Implement without using Arrays.sort method
-        return elements;
+        Comparable[] sortedElements = Arrays.copyOf(elements, elements.length);
+        randomizedQuickSort(sortedElements, 0, sortedElements.length - 1);
+        return sortedElements;
     }
+
+    private static Random integerRandomGenerator = new Random();
+
+    /**
+     * Sorts array/subarray of the comparable elements
+     * @param array array/subarray for sorting
+     * @param low first index of subarray (inclusive)
+     * @param high last index of subarray (inclusive)
+     */
+    private static void randomizedQuickSort(Comparable[] array, int low, int high) {
+        int randomizedMiddle;
+        if(low < high) {
+            randomizedMiddle = randomizedPartition(array, low, high);
+            randomizedQuickSort(array, low, randomizedMiddle - 1);
+            randomizedQuickSort(array, randomizedMiddle + 1, high);
+        }
+    }
+
+    private static int randomizedPartition(Comparable[] array, int low, int high) {
+        Comparable tmp;
+        int i;
+
+        i = integerRandomGenerator.nextInt(high - low) + low;
+        tmp = array[high];
+        array[high] = array[i];
+        array[i] = tmp;
+        return partition(array, low, high);
+    }
+
+    private static int partition(Comparable[] array, int low, int high) {
+        Comparable x;
+        Comparable tmp;
+        int i, j;
+
+        x = array[high];
+        i = low - 1;
+        for(j = low; j < high; j++) {
+            if (array[j].compareTo(x) <= 0) {
+                i++;
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+        tmp = array[high];
+        array[high] = array[i+1];
+        array[i+1] = tmp;
+
+        return i + 1;
+    }
+
 }
