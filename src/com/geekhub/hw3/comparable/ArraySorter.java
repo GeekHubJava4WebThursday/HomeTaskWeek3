2e7fb6a850d3daaf3d2a6d62a564a04cf5f74a91
vitaliy.kobrin <vitaliy.kobrin@gmail.com>
2015-10-23 17:48:06 +0300

    sorted arrays without Arrays.sort

--- src/com/geekhub/hw3/comparable/ArraySorter.java
+++ src/com/geekhub/hw3/comparable/ArraySorter.java
@@ -16,21 +16,44 @@ public class ArraySorter {
                 new User("Superman", "darkseid")
         };
 
-        Comparable[] sortedIntegers = sort(new Integer[]{2343, 2, 6, 1, 0, -1});
-        Comparable[] sortedStrings = sort(new String[]{ "abc", "54", "op" });
-        Comparable[] sortedUsers = sort(users);
+        Comparable[] sortedIntegers = sortInAscending(new Integer[]{2343, 2, 6, 1, 0, -1});
+        Comparable[] sortedStrings = sortInAscending(new String[]{"abc", "54", "op"});
+        Comparable[] sortedUsers = sortInDescending(users);
 
-        System.out.println("Sorted integers: " + Arrays.toString(sortedIntegers));
-        System.out.println("Sorted strings: " + Arrays.toString(sortedStrings));
-        System.out.println("Sorted users: " + Arrays.toString(sortedUsers));
+        System.out.println("Sorted integers: " + Arrays.toString(sortedIntegers)); //Result: [-1, 0, 1, 2, 6, 2343]
+        System.out.println("Sorted strings: " + Arrays.toString(sortedStrings)); //Result: [54, abc, op]
+        System.out.println("Sorted users: " + Arrays.toString(sortedUsers)); //Result: [Superman, Spider-Man, Programmer, Geekhub, Batman]
     }
 
     /**
      * Sorts array of the comparable elements
      * @return sorted array
      */
-    public static Comparable[] sort(Comparable[] elements) {
-        Arrays.sort(elements);
+    public static Comparable[] sortInAscending(Comparable[] elements) {
+        for(int i = 0; i < elements.length - 1; i++) {
+            for (int j = 0; j < elements.length - i - 1; j++) {
+                if (elements[j].compareTo(elements[j + 1]) > 0) {
+                    swap(elements, j);
+                }
+            }
+        }
         return elements;
+    }
+
+    public static Comparable[] sortInDescending(Comparable[] elements) {
+        for(int i = 0; i < elements.length - 1; i++) {
+            for (int j = 0; j < elements.length - i - 1; j++) {
+                if (elements[j].compareTo(elements[j + 1]) < 0) {
+                    swap(elements, j);
+                }
+            }
+        }
+        return elements;
+    }
+
+    private static void swap(Comparable[] objects, int index) {
+        Comparable o = objects[index];
+        objects[index] = objects[index + 1];
+        objects[index + 1] = o;
     }
 }
