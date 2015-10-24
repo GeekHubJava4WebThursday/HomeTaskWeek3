07845601d4d7828b2cb83f024e3f5e425f8bf189
Darchuk-Maxim <darchukmax@gmail.com>
2015-10-24 19:46:44 +0300

    Done arrays sort!

--- src/com/geekhub/hw3/comparable/ArraySorter.java
+++ src/com/geekhub/hw3/comparable/ArraySorter.java
@@ -7,8 +7,8 @@ public class ArraySorter {
 
     public static void main(String[] args) {
         Comparable[] sortedIntegers = sort(new Integer[]{2343, 2, 6, 1, 0, -1});
-        Comparable[] sortedStrings = sort(new String[]{ "abc", "54", "op" });
-
+        Comparable[] sortedStrings = sort(new String[]{ "abc", "54", "op"});
+        String s = "acb";
         System.out.println("Sorted integers: " + Arrays.toString(sortedIntegers));
         System.out.println("Sorted strings: " + Arrays.toString(sortedStrings));
     }
@@ -18,8 +18,19 @@ public class ArraySorter {
      * @return sorted array
      */
     public static Comparable[] sort(Comparable[] elements) {
-        Arrays.sort(elements);
-        return elements;
+        Comparable[] cloneComp = elements;
+        Comparable comparable = null;
+        for(int i = 0; i < cloneComp.length - 1; i++){
+            for(int j = i+1; j < cloneComp.length ; j++){
+                int res = cloneComp[i].compareTo(cloneComp[j]);
+                if(res > 0){
+                    comparable = cloneComp[i];
+                    cloneComp[i] = cloneComp[j];
+                    cloneComp[j] = comparable;
+                }
+            }
+        }
+        return cloneComp;
     }
 
 
